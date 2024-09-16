70ec7d15e2befd207572b19df2ed5dc0fdbc652a
Manishkumar1404 <110184621+Manishkumar1404@users.noreply.github.com>
2024-09-16 10:19:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,147 @@
+function rfun()
+{
+    document.getElementById("b1").style.color="red";
+}
+function bfun()
+{
+    document.getElementById("b1").style.color="black";
+}
+function blfun()
+{
+    document.getElementById("b1").style.color="blue";
+}
+function gfun()
+{
+    document.getElementById("b1").style.color="green";
+}
+function infun()
+{
+    document.getElementById("b1").style.color="indigo";
+}
+function yfun()
+{
+    document.getElementById("b1").style.color="yellow";
+}
+function ofun()
+{
+    document.getElementById("b1").style.color="orange";
+}
+function olfun()
+{
+    document.getElementById("b1").style.color="olivedrab";
+}
+function brfun()
+{
+    document.getElementById("b1").style.color="gray";
+}
+function vfun()
+{
+    document.getElementById("b1").style.color="rgb(60, 34, 115)";
+}
+function pfun()
+{
+    document.getElementById("b1").style.color="rgb(98, 23, 84)";
+}
+function grfun()
+{
+    document.getElementById("b1").style.color="rgb(35, 179, 19)";
+}
+function ashfun()
+{
+    document.getElementById("b1").style.color="rgb(74, 72, 70)";
+}
+function bbfun()
+{
+    document.getElementById("b1").style.color="rgb(40, 23, 5)";
+}
+function lpfun()
+{
+    document.getElementById("b1").style.color="rgb(223, 159, 143)";
+}
+
+
+function r1fun()
+{
+    document.getElementById("b1").style.backgroundColor="red";
+}
+function b1fun()
+{
+    document.getElementById("b1").style.backgroundColor="black";
+}
+function bl1fun()
+{
+    document.getElementById("b1").style.backgroundColor="blue";
+}
+function g1fun()
+{
+    document.getElementById("b1").style.backgroundColor="green";
+}
+function in1fun()
+{
+    document.getElementById("b1").style.backgroundColor="indigo";
+}
+function y1fun()
+{
+    document.getElementById("b1").style.backgroundColor="yellow";
+}
+function o1fun()
+{
+    document.getElementById("b1").style.backgroundColor="orange";
+}
+function ol1fun()
+{
+    document.getElementById("b1").style.backgroundColor="olivedrab";
+}
+function br1fun()
+{
+    document.getElementById("b1").style.backgroundColor="gray";
+}
+function v1fun()
+{
+    document.getElementById("b1").style.backgroundColor="rgb(60, 34, 115)";
+}
+function p1fun()
+{
+    document.getElementById("b1").style.backgroundColor="rgb(98, 23, 84)";
+}
+function gr1fun()
+{
+    document.getElementById("b1").style.backgroundColor="rgb(35, 179, 19)";
+}
+function ash1fun()
+{
+    document.getElementById("b1").style.backgroundColor="rgb(74, 72, 70)";
+}
+function bb1fun()
+{
+    document.getElementById("b1").style.backgroundColor="rgb(40, 23, 5)";
+}
+function lp1fun()
+{
+    document.getElementById("b1").style.backgroundColor="rgb(223, 159, 143)";
+}
+function ft1fun()
+{
+    document.getElementById("b1").style.fontFamily="'Courier New', Courier, monospace";
+}
+function ft2fun()
+{
+    document.getElementById("b1").style.fontFamily="'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif";
+}
+function ft3fun()
+{
+    document.getElementById("b1").style.fontFamily="'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif";
+}
+function ft4fun()
+{
+    document.getElementById("b1").style.fontFamily="'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif";
+}
+function ft5fun()
+{
+    document.getElementById("b1").style.fontFamily="''Times New Roman', Times, serif";
+}
+function ft6fun()
+{
+    document.getElementById("b1").style.fontFamily="Arial, Helvetica, sans-serif";
+}
+
